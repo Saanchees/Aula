1b3ee58c1c46d782c4359cf0c50958e969d01d1f
Lucas de Souza Sanches <1802231@SALAS.AULAS>
2019-03-21 10:34:31 -0300

    reconfig

--- teste.py
+++ teste.py
@@ -2,6 +2,6 @@ import pytest
 from principal import soma
 
 def test_soma():
-    assert soma(3+5)==8
-    assert soma(8-4)==4
-    assert soma(0+5)==5
+    assert soma(3,5)==8
+    assert soma(8,4)==4
+    assert soma(0,5)==5
